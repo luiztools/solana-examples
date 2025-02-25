c062059acd108bbf957637b22e707a2523ac0694
LuizTools <luizfduartejr@gmail.com>
2025-02-25 19:43:00 -0300

    wallet-solana part 2

--- wallet-solana/WalletService.js
+++ wallet-solana/WalletService.js
@@ -1,38 +1,51 @@
-const { Keypair, PublicKey, Connection, clusterApiUrl, LAMPORTS_PER_SOL } = require("@solana/web3.js");
+const { Keypair, PublicKey, Connection, clusterApiUrl, LAMPORTS_PER_SOL, Transaction, sendAndConfirmTransaction, SystemProgram } = require("@solana/web3.js");
+const { binary_to_base58, base58_to_binary } = require("base58-js");
+
 const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 
 let myWallet = null;
 
-//const publicKey = new PublicKey(wallet.publicKey);
-//const secretKey = wallet.secretKey;
-
 function createWallet() {
     myWallet = new Keypair();
+    myWallet.publicKeyDecoded = myWallet.publicKey.toBase58();
+    myWallet.secretKeyDecoded = binary_to_base58(myWallet.secretKey);
     return myWallet;
 }
 
-module.exports = {
-    createWallet
+function recoverWallet(secretKey) {
+    const binarySecretKey = base58_to_binary(secretKey);
+    myWallet = Keypair.fromSecretKey(binarySecretKey);
+    myWallet.publicKeyDecoded = myWallet.publicKey.toBase58();
+    myWallet.secretKeyDecoded = secretKey;
+    return myWallet;
 }
 
 async function getBalance() {
-    try {
-        
-        const balance = await connection.getBalance(publicKey);
-        console.log("Your balance is " + balance);
-    } catch (err) {
-        console.error(err);
+    const balance = await connection.getBalance(new PublicKey(myWallet.publicKey));
+    return {
+        lamports: balance,
+        sol: balance / LAMPORTS_PER_SOL
     }
 }
 
-//1 SOL = 1 bilhão de Lamports
-async function getSol() {
-    try {
-        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-        const tx = await connection.requestAirdrop(publicKey, 2 * LAMPORTS_PER_SOL);
-        await connection.confirmTransaction({ signature: tx });
-    }
-    catch (err) {
-        console.error(err);
-    }
+async function transfer(to, amountInSol) {
+    const transaction = new Transaction();
+
+    const sendSolInstruction = SystemProgram.transfer({
+        fromPubkey: new PublicKey(myWallet.publicKey),
+        toPubkey: new PublicKey(to),
+        lamports: LAMPORTS_PER_SOL * amountInSol,
+    });
+
+    transaction.add(sendSolInstruction);
+
+    const txHash = await sendAndConfirmTransaction(connection, transaction, [myWallet]);
+    return txHash;
+}
+
+module.exports = {
+    createWallet,
+    recoverWallet,
+    getBalance,
+    transfer
 }
--- wallet-solana/index.js
+++ wallet-solana/index.js
@@ -18,10 +18,67 @@ function createWallet() {
     console.clear();
 
     const myWallet = WalletService.createWallet();
-    myAddress = myWallet.publicKey.toBase58();
+    myAddress = myWallet.publicKeyDecoded;
 
     console.log(`Your new wallet:`);
     console.log(myAddress);
+    console.log(`Your secret key:`);
+    console.log(myWallet.secretKeyDecoded);
+
+    preMenu();
+}
+
+function recoverWallet() {
+    console.clear();
+    rl.question(`What is your secret key? `, (secretKey) => {
+        const myWallet = WalletService.recoverWallet(secretKey);
+        myAddress = myWallet.publicKeyDecoded;
+
+        console.log(`Your recovered wallet:`);
+        console.log(myAddress);
+
+        preMenu();
+    })
+}
+
+async function getBalance() {
+    console.clear();
+
+    if (!myAddress) {
+        console.log(`You don't have a wallet yet.`);
+        return preMenu();
+    }
+
+    const { lamports, sol } = await WalletService.getBalance();
+    console.log(`SOL ${sol}`);
+
+    preMenu();
+}
+
+function sendTx() {
+    console.clear();
+
+    if (!myAddress) {
+        console.log(`You don't have a wallet yet.`);
+        return preMenu();
+    }
+
+    console.log(`Your wallet is ${myAddress}`);
+    rl.question(`To Wallet: `, (toWallet) => {
+
+        rl.question(`Amount (in SOL): `, async (amountInSol) => {
+            if (!amountInSol) {
+                console.log(`Invalid amount.`);
+                return preMenu();
+            }
+
+            const txHash = await WalletService.transfer(toWallet, amountInSol);
+            console.log("Transaction successful: ");
+            console.log(txHash);
+
+            return preMenu();
+        })
+    })
 
     preMenu();
 }
@@ -39,14 +96,12 @@ function menu() {
         console.log("2 - Recover Wallet");
         console.log("3 - Balance");
         console.log("4 - Send SOL");
-        console.log("5 - Search tx");
         rl.question("Choose your option: ", (answer) => {
             switch (answer) {
                 case "1": createWallet(); break;
                 case "2": recoverWallet(); break;
                 case "3": getBalance(); break;
                 case "4": sendTx(); break;
-                case "5": searchTx(); break;
                 default: {
                     console.log('Wrong option!');
                     menu();
